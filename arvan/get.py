a15bf9d05aca1c98a4d08147545e62e463166e5c
Hessam <hessam.hashemi21@gmail.com>
2020-01-31 17:35:23 +0330

    added complete get for the video

--- arvan/get.py
+++ arvan/get.py
@@ -5,15 +5,45 @@ import requests
 class GetVideoMixin(object):
     """Handle uploading a new temporary file to the ArvanCloud API."""
 
+    def get_video(self, video_id):
+        """
+        Getting the whole Json of video information using Video ID
+        Args:
+        """
+        endpoint = 'https://napi.arvancloud.com/vod/2.0/videos/' + video_id
+        header = {'Authorization': self.token}
+
+        return requests.get(url=endpoint, headers=header).json()['data']
+
     def get_hls(self, video_id):
         """
-        Getting the HLS link using File ID
+        Getting the HLS link using Video ID
         Args:
         """
         endpoint = 'https://napi.arvancloud.com/vod/2.0/videos/' + video_id
         header = {'Authorization': self.token}
 
         return requests.get(url=endpoint, headers=header).json()['data']['hls_playlist']
+
+    def get_video_url(self, video_id):
+        """
+        Getting the Video Source link using Video ID
+        Args:
+        """
+        endpoint = 'https://napi.arvancloud.com/vod/2.0/videos/' + video_id
+        header = {'Authorization': self.token}
+
+        return requests.get(url=endpoint, headers=header).json()['data']['video_url']
+
+    def get_player_url(self, video_id):
+        """
+        Getting the Player link using Video ID
+        Args:
+        """
+        endpoint = 'https://napi.arvancloud.com/vod/2.0/videos/' + video_id
+        header = {'Authorization': self.token}
+
+        return requests.get(url=endpoint, headers=header).json()['data']['player_url']
 
 
 class GetMixin(GetVideoMixin):
